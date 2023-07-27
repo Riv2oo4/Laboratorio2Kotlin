eefc59a637de5f23c8fade16465690913bda350b
Riv2oo4 <riv22500@uvg.edu.gt>
2023-07-26 22:10:23 -0600

    Actualización

--- Ejercicios.kt
+++ Ejercicios.kt
@@ -14,7 +14,15 @@ fun isPalindrome(input: String) : Boolean {
 fun performOperation(a: Int, b: Int, operation:(Int, Int)-> Int): Int{
     return operation (a,b)
 }
-
+// EJERCICIO 6
+data class Person(val name: String, val age: Int, val gender: String)
+data class Student(val name:String, val age: Int, val gender: String, val studentId: String)
+fun mapeo(personas: List<Person>): List<Student>{
+    return personas.map{person ->
+        val studentId = "S${person.name.take(2)}${person.age}"
+        Student(person.name, person.age, person.gender, studentId)
+    }
+}
 fun main() {
     val numeros = listOf(1, 2, 3, 4, 5)
     val promedio = calcularPromedio(numeros)
@@ -34,4 +42,11 @@ fun main() {
     val suma = {x: Int, y: Int -> x+y}
     val resultado = performOperation(4,3, suma)
     println("Resultado: $resultado")
+
+    //Ejercicio 6
+    val listaPersonas = listOf( Person("Hugo",19,"hombre"),Person("Eduardo",20,"hombre"))
+    val listaEstudiantes = mapeo(listaPersonas)
+
+    listaEstudiantes.forEach{Student -> println("El Estudiante ${Student.name} tiene ${Student.age} años")}
 }
+
